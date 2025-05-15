bfd2ab2809a20beed6f9dcd384a4402a416849f6
abhipriyadarshi <abhi.priyadarshi@gmail.com>
2025-05-15 07:54:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,18 @@
-provider "aws" {
-region = "us-east-1"
+provider "aws"{
+region "us-east-1"
 }
-resourse "aws_instance" {
-ami = "ami-00a929b66ed6e0de6"
-instance_type = "t2.micro"
-tag {
-Name = "hcp-server"
+
+resourse "aws_instance" "One"{
+count = 4
+instance_type = "t2_micro"
+key_name = "my_amazon1"
+vpc_security_group_ids = "vpc-0fffc376be2a507df"
+tags = {
+Name = var.instance_name[count.index]
 }
+
+}
+
+variable  "instance_name" {
+default = ["jenkins","app-server1","app-server2","monitoring"]
 }
