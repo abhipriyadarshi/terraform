28b0f57b1d3dd11837658abcd20ca0173dc09695
abhipriyadarshi <abhi.priyadarshi@gmail.com>
2025-05-15 07:57:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ region "us-east-1"
 
 resourse "aws_instance" "One"{
 count = 4
+ami = "ami-085386e29e44dacd7"
 instance_type = "t2_micro"
 key_name = "my_amazon1"
 vpc_security_group_ids = "vpc-0fffc376be2a507df"
